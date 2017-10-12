15516db4e39ce00b55d9ade28e39e3b5be94e5c0
Cory <Link2cory@gmail.com>
2017-10-11 19:41:59 -0700

    added support for shared gpio ports

--- src/stepper.c
+++ src/stepper.c
@@ -5,11 +5,11 @@
 *******************************************************************************/
 void stepper_construct(stepper_attr_t config) {
   *config.dir_port = 0;
-  *config.dir_port_ddr = config.dir_pin;
+  *config.dir_port_ddr |= config.dir_pin;
 
   *config.enable_port = 0;
-  *config.enable_port_ddr = config.enable_pin;
+  *config.enable_port_ddr |= config.enable_pin;
 
   *config.step_port = 0;
-  *config.step_port_ddr = config.step_pin;
+  *config.step_port_ddr |= config.step_pin;
 }
--- test/test_stepper.c
+++ test/test_stepper.c
@@ -27,15 +27,15 @@ void setUp(void)
   srand(time(NULL));
 
   dir_port = (uint8_t)rand();
-  dir_port_ddr = (uint8_t)rand();
+  dir_port_ddr = 0;
   dir_pin = (1 << (rand() % 8 + 0));
 
   enable_port = (uint8_t)rand();
-  enable_port_ddr = (uint8_t)rand();
+  enable_port_ddr = 0;
   enable_pin = (1 << (rand() % 8 + 0));
 
   step_port = (uint8_t)rand();
-  step_port_ddr = (uint8_t)rand();
+  step_port_ddr = 0;
   step_pin = (1 << (rand() % 8 + 0));
 }
 
@@ -46,7 +46,7 @@ void tearDown(void)
 /*******************************************************************************
 * Tests
 *******************************************************************************/
-void test_construct(void)
+void test_construct_initializes_hardware_pins_with_individual_ports(void)
 {
   stepper_attr_t config;
   config.dir_port = &dir_port;
@@ -71,4 +71,28 @@ void test_construct(void)
 
   TEST_ASSERT(step_port == 0);
   TEST_ASSERT(step_port_ddr == step_pin);
+}
+
+void test_construct_initializes_hardware_pins_with_shared_ports(void)
+{
+  stepper_attr_t config;
+  uint8_t shared_port;
+  uint8_t shared_port_ddr;
+
+  config.dir_port = &shared_port;
+  config.dir_port_ddr = &shared_port_ddr;
+  config.dir_pin = 1;
+
+  config.enable_port = &shared_port;
+  config.enable_port_ddr = &shared_port_ddr;
+  config.enable_pin = 2;
+
+  config.step_port = &shared_port;
+  config.step_port_ddr = &shared_port_ddr;
+  config.step_pin = 4;
+
+  stepper_construct(config);
+
+  TEST_ASSERT(shared_port_ddr == 7);
+  TEST_ASSERT(shared_port == 0);
 }
